79d695603cc0c5d37b1b7a43d1249b67b8acd94a
Martin Budden <mjbudden@gmail.com>
2026-03-02 02:24:03 +0000

    Corrected debug.h

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,9 @@
-#if defined (LIBRARY_TASK_BASE_USE_EMPTY_MAIN)
+#if defined(LIBRARY_TASK_UTILITIES_USE_EMPTY_MAIN)
+
+#include "debug.h"
+#include "message_queue_base.h"
+#include "task_base.h"
+#include "time_microseconds.h"
 
 int main()
 {
